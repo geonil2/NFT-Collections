8797fa2b1761a8d0169968f29e293db84caf3274
kimgeonil <kji8922@naver.com>
2022-07-07 19:07:42 +0900

    Test CI/CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-REPOSITORY=/home/ubuntu/nft-collections-front
-
-cd $REPOSITORY
-sudo apt-get update
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16.14.2
-rm -rf node_modules
-npm install
-npm install pm2 -g
-npm run build
-pm2 reload 0
+##!/bin/bash
+#REPOSITORY=/home/ubuntu/nft-collections-front
+#
+#cd $REPOSITORY
+#sudo apt-get update
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+#. ~/.nvm/nvm.sh
+#nvm install 16.14.2
+#rm -rf node_modules
+#npm install
+#npm install pm2 -g
+#npm run build
+#pm2 reload 0
 
