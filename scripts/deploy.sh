2c80f99aac560021fd03c3f3ee874faa58242ce5
kimgeonil <kji8922@naver.com>
2022-07-07 18:12:09 +0900

    Test CI/CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 REPOSITORY=/home/ubuntu/nft-collections-front
 
 cd $REPOSITORY
-/.nvm/versions/node/v16.14.2/bin/pm2 start npm --name nft-collections-front -- start
+pm2 start npm --name nft-collections-front -- start
