dcb2ee956dd65adc179e844e9d4cbb888c67f929
kimgeonil <kji8922@naver.com>
2022-07-07 18:51:26 +0900

    Test CI/CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,5 @@
 REPOSITORY=/home/ubuntu/nft-collections-front
 
 cd $REPOSITORY
-sudo apt-get update
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16.14.2
-npm install pm2 -g
-pm2 start npm --name nft-collections-front -- start
+pm2 reload 0
 
